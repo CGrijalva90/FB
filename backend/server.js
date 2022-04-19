d5b14a12a1d0224d7917577b937ef3c34c520955
Carlos Grijalva <grijalva.carlos@gmail.com>
2022-04-19 13:48:20 -0400

    Add basic frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const options = {
+  origin: "http://localhost:3000",
+  useSuccessStatus: 200,
+};
+
+app.use(cors(options));
+
+app.get("/", (req, res) => {
+  res.send("Welcome from home");
+});
+
+app.get("/books", (req, res) => {
+  res.send("Welcome from books");
+});
+
+app.listen(8000, () => {
+  console.log("server is listening");
+});
