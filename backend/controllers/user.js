f9830cf508a7c324ee8c961a3ebdc84ed7bbec1d
Carlos Grijalva <grijalva.carlos@gmail.com>
2022-04-20 16:31:41 -0400

    Add login functionality

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -7,6 +7,8 @@ const {
 const bcrypt = require("bcrypt");
 const { generateToken } = require("../helpers/tokens");
 const { sendVerificationEmail } = require("../helpers/mailer");
+const jwt = require("jsonwebtoken");
+const e = require("express");
 
 exports.register = async (req, res) => {
   try {
@@ -87,6 +89,58 @@ exports.register = async (req, res) => {
       token,
       verified: user.verified,
       message: "Register Successful! Please activate your email",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.activateAccount = async (req, res) => {
+  try {
+    const { token } = req.body;
+    const user = jwt.verify(token, process.env.TOKEN_SECRET);
+    const check = await User.findById(user.id);
+    if (check.verified == true) {
+      return res.status(400).json({
+        message: "This email is already activated",
+      });
+    } else {
+      await User.findByIdAndUpdate(user.id, { verified: true });
+      return res.status(200).json({
+        message: "Account activated!",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message:
+          "This email address you entered is not connected to an account.",
+      });
+    }
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        message: "Invalid credentials, please try again!",
+      });
+    }
+    const token = generateToken({ id: user._id.toString() }, "7d");
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token,
+      verified: user.verified,
+      message: "Login Successful!",
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
