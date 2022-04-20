410443751673545282199fa25829c4adbe04d3c4
Carlos Grijalva <grijalva.carlos@gmail.com>
2022-04-20 14:05:08 -0400

    added username validation

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,74 @@
-exports.home = (req, res) => {
-  res.status(200).json({
-    message: "from user",
-  });
+const User = require("../models/User");
+const {
+  validateEmail,
+  validateLength,
+  validateUsername,
+} = require("../helpers/validation");
+const bcrypt = require("bcrypt");
+const { validate } = require("../models/User");
+
+exports.register = async (req, res) => {
+  try {
+    const {
+      first_name,
+      last_name,
+      email,
+      password,
+      username,
+      bYear,
+      bMonth,
+      bDay,
+      gender,
+    } = req.body;
+    3;
+
+    if (!validateEmail(email)) {
+      return res.status(400).json({ message: "Invalid email address" });
+    }
+
+    if (!validateLength(first_name, 3, 30)) {
+      return res.status(400).json({
+        message: "First name must be between 3 and 30 characters",
+      });
+    }
+
+    if (!validateLength(last_name, 3, 30)) {
+      return res.status(400).json({
+        message: "Last name must be between 3 and 30 characters",
+      });
+    }
+    if (!validateLength(password, 6, 40)) {
+      return res.status(400).json({
+        message: "Password must be at least 6 characters",
+      });
+    }
+
+    const check = await User.findOne({ email });
+
+    if (check) {
+      return res.status(400).json({
+        message:
+          "The email address already exists, try with a different email address",
+      });
+    }
+
+    const cryptedPassword = await bcrypt.hash(password, 12);
+    const tempUsername = first_name + last_name;
+    let newUsername = await validateUsername(tempUsername);
+
+    const user = await new User({
+      first_name,
+      last_name,
+      email,
+      password: cryptedPassword,
+      username: newUsername,
+      bYear,
+      bMonth,
+      bDay,
+      gender,
+    }).save();
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
--- backend/models/User.js
+++ backend/models/User.js
@@ -2,139 +2,143 @@ const mongoose = require("mongoose");
 
 const { ObjectId } = mongoose.Schema;
 
-const userSchema = mongoose.Schema({
-  first_name: {
-    type: String,
-    required: [true, "first name is required"],
-    trim: true,
-    text: true,
-  },
-  last_name: {
-    type: String,
-    required: [true, "last name is required"],
-    trim: true,
-    text: true,
-  },
-  username: {
-    type: String,
-    required: [true, "username is required"],
-    trim: true,
-    text: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: [true, "email is required"],
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: [true, "password is required"],
-  },
-  picture: {
-    type: String,
-    default:
-      "https://res.cloudinary.com/dmhcnhtng/image/upload/v1643044376/avatars/default_pic_jeaybr.png",
-  },
-  cover: {
-    type: String,
-    trim: true,
-  },
-  gender: {
-    type: String,
-    required: [true, "gender is required"],
-    trime: true,
-  },
-  verified: {
-    type: Boolean,
-    default: false,
-  },
-  bDay: {
-    type: Number,
-    required: true,
-    trime: true,
-  },
-  bYear: {
-    type: Number,
-    required: true,
-    trime: true,
-  },
-  bMonth: {
-    type: Number,
-    required: true,
-    trime: true,
-  },
-  friends: {
-    type: Array,
-    default: [],
-  },
-  following: {
-    type: Array,
-    default: [],
-  },
-  followers: {
-    type: Array,
-    default: [],
-  },
-  requests: {
-    type: Array,
-    default: [],
-  },
-  search: [
-    {
-      user: {
-        type: ObjectId,
-        ref: "User",
+const userSchema = mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: [true, "first name is required"],
+      trim: true,
+      text: true,
+    },
+    last_name: {
+      type: String,
+      required: [true, "last name is required"],
+      trim: true,
+      text: true,
+    },
+    username: {
+      type: String,
+      required: [true, "username is required"],
+      trim: true,
+      text: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    picture: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dmhcnhtng/image/upload/v1643044376/avatars/default_pic_jeaybr.png",
+    },
+    cover: {
+      type: String,
+      trim: true,
+    },
+    gender: {
+      type: String,
+      required: [true, "gender is required"],
+      trime: true,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    bDay: {
+      type: Number,
+      required: true,
+      trime: true,
+    },
+    bYear: {
+      type: Number,
+      required: true,
+      trime: true,
+    },
+    bMonth: {
+      type: Number,
+      required: true,
+      trime: true,
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    requests: {
+      type: Array,
+      default: [],
+    },
+    search: [
+      {
+        user: {
+          type: ObjectId,
+          ref: "User",
+        },
+      },
+    ],
+    details: {
+      bio: {
+        type: String,
+      },
+      otherName: {
+        type: String,
+      },
+      job: {
+        type: String,
+      },
+      workplace: {
+        type: String,
+      },
+      highSchool: {
+        type: String,
+      },
+      college: {
+        type: String,
+      },
+      currentCity: {
+        type: String,
+      },
+      hometown: {
+        type: String,
+      },
+      relationship: {
+        type: String,
+        enum: ["Single", "In a relationship", "Married", "Divorced"],
+      },
+      instagram: {
+        type: String,
       },
     },
-  ],
-  details: {
-    bio: {
-      type: String,
-    },
-    otherName: {
-      type: String,
-    },
-    job: {
-      type: String,
-    },
-    workplace: {
-      type: String,
-    },
-    highSchool: {
-      type: String,
-    },
-    college: {
-      type: String,
-    },
-    currentCity: {
-      type: String,
-    },
-    hometown: {
-      type: String,
-    },
-    relationship: {
-      type: String,
-      enum: ["Single", "In a relationship", "Married", "Divorced"],
-    },
-    instagram: {
-      type: String,
-    },
-  },
-  savedPosts: [
-    {
-      post: {
-        type: ObjectId,
-        ref: "Post",
+    savedPosts: [
+      {
+        post: {
+          type: ObjectId,
+          ref: "Post",
+        },
+        savedAt: {
+          type: Date,
+          default: new Date(),
+        },
       },
-      savedAt: {
-        type: Date,
-        default: new Date();
-      }
-    },
-  ],
-}, {
-  timestamps: true
-});
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
