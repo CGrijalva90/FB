8552781d3c589e7196d6a36a8997379c07595ee7
Carlos Grijalva <grijalva.carlos@gmail.com>
2022-04-20 19:34:32 -0400

    Add login frontend and set up redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
+
 function App() {
-  const get = async () => {
-    const res = await fetch("http://localhost:8000");
-    console.log(res);
-  };
-  get();
-  return <div>welcome to frontend </div>;
+  return (
+    <div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} exact />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
