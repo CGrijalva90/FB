421ff9d8571af7b279c671ac21352e58d503c3c3
Carlos Grijalva <grijalva.carlos@gmail.com>
2022-04-21 11:26:44 -0400

    Added input validation and styling

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -3,7 +3,7 @@ import "./styles.css";
 import { Formik, Form } from "formik";
 import { Link } from "react-router-dom";
 import LoginInput from "../../components/inputs/loginInput";
-
+import * as Yup from "yup";
 const loginInfos = {
   email: "",
   password: "",
@@ -11,12 +11,20 @@ const loginInfos = {
 const Login = () => {
   const [login, setLogin] = useState(loginInfos);
   const { email, password } = login;
-  console.log(login);
+
   const handleLoginChange = (e) => {
     const { name, value } = e.target;
     setLogin({ ...login, [name]: value });
   };
-  console.log(login);
+
+  const loginValidation = Yup.object({
+    email: Yup.string()
+      .required("Email address is required")
+      .email("Must be a valid email.")
+      .max(100),
+    password: Yup.string().required("Password is required"),
+  });
+
   return (
     <div className="login">
       <div className="login_wrapper">
@@ -29,7 +37,11 @@ const Login = () => {
           </div>
           <div className="login_2">
             <div className="login_2_wrap">
-              <Formik enableReinitialize initialValues={{ email, password }}>
+              <Formik
+                enableReinitialize
+                initialValues={{ email, password }}
+                validationSchema={loginValidation}
+              >
                 {(formik) => (
                   <Form>
                     <LoginInput
@@ -43,6 +55,7 @@ const Login = () => {
                       name="password"
                       placeholder="Password"
                       onChange={handleLoginChange}
+                      bottom
                     />
                     <button className="blue_btn">Log In</button>
                   </Form>
