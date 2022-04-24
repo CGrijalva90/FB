aede5977d36b6570712577fca77daa141da93dbc
Carlos Grijalva <grijalva.carlos@gmail.com>
2022-04-24 16:50:37 -0400

    Protect routes on the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginForm from "./components/login/LoginForm";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
+import LoggedInRoutes from "./routes/LoggedInRoutes";
+import NotLoggedInRoutes from "./routes/NotLoggedInRoutes";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} exact />
+        <Route element={<LoggedInRoutes />}>
+          <Route path="/profile" element={<Profile />} exact />
+          <Route path="/" element={<Home />} exact />
+        </Route>
+        <Route element={<NotLoggedInRoutes />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
       </Routes>
     </div>
   );
